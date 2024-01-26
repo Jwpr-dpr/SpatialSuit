a4923ab8d4b1253dc89ee9dc8280c01af354f88f
Jhon Wilmer Pino Román <124304943+Jwpr-dpr@users.noreply.github.com>
2024-01-25 22:27:44 -0500

    Upload files for SpatialSuit app

---
+++ build/CMakeFiles/exampleED.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exampleED.dir/exampleED.cc.o"
+  "CMakeFiles/exampleED.dir/exampleED.cc.o.d"
+  "CMakeFiles/exampleED.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/exampleED.dir/src/ActionInitialization.cc.o.d"
+  "CMakeFiles/exampleED.dir/src/EDDetectorConstruction.cc.o"
+  "CMakeFiles/exampleED.dir/src/EDDetectorConstruction.cc.o.d"
+  "CMakeFiles/exampleED.dir/src/EventAction.cc.o"
+  "CMakeFiles/exampleED.dir/src/EventAction.cc.o.d"
+  "CMakeFiles/exampleED.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/exampleED.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/exampleED.dir/src/RunAction.cc.o"
+  "CMakeFiles/exampleED.dir/src/RunAction.cc.o.d"
+  "CMakeFiles/exampleED.dir/src/SensitiveDetector.cc.o"
+  "CMakeFiles/exampleED.dir/src/SensitiveDetector.cc.o.d"
+  "exampleED"
+  "exampleED.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exampleED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
